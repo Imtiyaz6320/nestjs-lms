6b02b8ce726649149a2a321675c9d72f63078d8a
imtiyaz-Aayaninfotech <imtiyaz.hussain@aayaninfotech.com>
2025-10-23 16:57:05 +0530

    auth guard implement

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,32 +1,3 @@
-// import { Injectable } from '@nestjs/common';
-// import { UserService } from 'src/user/user.service';
-// import { RegisterDto } from './dto/registerUser.dto';
-// import bcrypt from "bcrypt";
-// import { JwtService } from '@nestjs/jwt';
-
-// @Injectable()
-// export class AuthService {
-//     constructor(private readonly userService: UserService,
-//         private readonly jwtService: JwtService,
-//     ) { }
-//     async registerUser(registerUserDto: RegisterDto) {
-//         const saltRounds = 10
-//         const hash = await bcrypt.hash(registerUserDto.password, saltRounds)
-//         const user = await this.userService.createUser({
-//             ...registerUserDto,
-//             password: hash,
-//         });
-//         // return user
-
-//         const payload = { sub: user._id};
-//         const token = await this.jwtService.signAsync(payload)
-//         console.log(token);
-//         return ({secretToken :token}) 
-
-//     }
-// }
-
-
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterDto } from './dto/registerUser.dto';
